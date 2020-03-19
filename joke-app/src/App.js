e4fd9230fea5b5475d8fc2b8fbaa867fa917df30
Jason-Kyle De Lara <jason8oh8@gmail.com>
2020-03-19 00:55:21 -0700

    jokes api made

--- joke-app/src/App.js
+++ joke-app/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+// import './App.css';
+import './jokes.css';
+
+
 // import Joke from './Components/Joke'
 
 class App extends React.Component {
@@ -7,27 +11,38 @@ class App extends React.Component {
     this.state ={
         joke: {}
     }
+    this.getJoke = this.getJoke.bind(this);
+    this.sendToTwitter = this.sendToTwitter.bind(this);
   }
-  componentDidMount(){
-    const jokeAPI = 'https://official-joke-api.appspot.com/random_joke';
-    // const sw = 'https://swapi.co/api/people/1';
-
-    let joke = fetch(jokeAPI)
-      .then( res => res.json())
+  async getJoke() {
+    await fetch('https://official-joke-api.appspot.com/random_joke')
+      .then(res => res.json())
       .then(data => {
         console.log(data);
         this.setState({
           joke: data
         })
       })
-        
-    
+  }
+  componentDidMount(){
+    this.getJoke();
+  }
+
+  sendToTwitter(){
+    console.log('somejoke')
   }
   render(){
     return (
       <div className="App">
+        <div className="line"></div>
+        <div className="child">
         <h1>{this.state.joke.setup}</h1>
         <h4>{this.state.joke.punchline}</h4>
+
+        <a href="#" onClick={this.getJoke}>New Joke</a>
+        <a href="#" onClick={this.sendToTwitter}>LMAO</a>
+        </div>
+        
       </div>
     );
   }
